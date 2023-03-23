f061b9202783cc5cea283c906cb7ee6c51963cb8
levymc <levymcruz@gmail.com>
2023-03-23 17:01:27 -0300

    App pronto

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.config import Config
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class Registro(Base):
+    __tablename__ = 'registros'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    sobrenome = Column(String)
+    departamento = Column(String)
+    codigo = Column(String)
+    usuario = Column(String)
+    senha = Column(String)
+
 
 class Formulario(BoxLayout):
     def __init__(self, **kwargs):
@@ -34,6 +51,35 @@ class Formulario(BoxLayout):
         btn_salvar = Button(text='Salvar', size_hint=(1, 0.3))
         btn_salvar.bind(on_press=lambda _: self.salvar())  # <--- Linha adicionada
         self.add_widget(btn_salvar)
+        
+    def salvar(self):
+        # Criando objeto de conexão com o banco de dados
+        engine = create_engine('sqlite:///cadastro.db', echo=True)
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        # Criando objeto de registro
+        registro = Registro(
+            nome=self.nome.text,
+            sobrenome=self.sobrenome.text,
+            departamento=self.departamento.text,
+            codigo=self.codigo.text,
+            usuario=self.usuario.text,
+            senha=self.senha.text
+        )
+
+        # Adicionando registro ao banco de dados
+        session.add(registro)
+        session.commit()
+
+        # Limpando campos do formulário
+        self.nome.text = ''
+        self.sobrenome.text = ''
+        self.departamento.text = ''
+        self.codigo.text = ''
+        self.usuario.text = ''
+        self.senha.text = ''
 
 
 class CadastroApp(App):
@@ -45,4 +91,3 @@ if __name__ == '__main__':
     Config.set('graphics', 'width', '400')
     Config.set('graphics', 'height', '600')
     CadastroApp().run()
-
